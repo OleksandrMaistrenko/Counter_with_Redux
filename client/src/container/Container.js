e38e45c360d59489e0f4ca6ee954f8fef7a2961d
Oleksandr Maistrenko <oleksandrmaistrenko100@gmail.com>
2024-03-21 14:46:37 +0100

    fix reset

--- client/src/container/Container.js
+++ client/src/container/Container.js
@@ -1,7 +1,7 @@
 import "./Container.css";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./counterSlice";
+import { decrement, increment, reset } from "./counterSlice";
 
 function Container() {
   const buttonStyle = { backgroundColor: "lightgreen" };
