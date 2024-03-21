f4049cbe330445366fddd5225c9a15e9421909ef
Oleksandr Maistrenko <oleksandrmaistrenko100@gmail.com>
2024-03-21 14:38:00 +0100

    first commit

---
+++ client/src/container/Container.js
@@ -0,0 +1,43 @@
+import "./Container.css";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+
+function Container() {
+  const buttonStyle = { backgroundColor: "lightgreen" };
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="container">
+      <div className="main">
+        <div>
+          <button
+            onClick={() => dispatch(decrement())}
+            className="btn font_size_80"
+          >
+            -
+          </button>
+        </div>
+        <div className="font_size_80">{count}</div>
+        <div>
+          <button
+            onClick={() => dispatch(increment())}
+            className="btn font_size_80"
+          >
+            +
+          </button>
+        </div>
+      </div>
+      {!!count && (
+        <div onClick={() => dispatch(reset())} className="reset_container">
+          <button className="btn_reset" style={buttonStyle}>
+            RESET
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Container;
